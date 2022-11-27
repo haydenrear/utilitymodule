b3030436b24a470a650b6cd33e6301877ceefbcd
Hayden Rear <hayden.rear@gmail.com>
2022-11-27 15:12:23 -0500

    some passed

--- src/test/java/com/hayden/utilitymodule/IndArrayUtilsTest.java
+++ src/test/java/com/hayden/utilitymodule/IndArrayUtilsTest.java
@@ -1,25 +1,17 @@
 package com.hayden.utilitymodule;
 
-import com.hayden.utilitymodule.scaling.VectorizedMap;
-import jdk.incubator.vector.*;
 import org.datavec.api.util.ndarray.RecordConverter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.api.DataSetUtil;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.INDArrayIndex;
 import org.nd4j.linalg.indexing.NDArrayIndex;
-import org.nd4j.linalg.util.DataSetUtils;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 
 class IndArrayUtilsTest {
-    static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_128;
-
 
     @Test
     public void testINDArrayIndex() {
