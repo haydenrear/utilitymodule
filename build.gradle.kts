53fb0a15af7e3962a66cfa115a26738d48909c0a
Hayden Rear <hayden.rear@gmail.com>
2023-04-07 08:18:50 -0400

    before change

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,16 @@
 
 plugins {
     id("com.hayden.java-conventions")
+    kotlin("jvm") version "1.8.20"
 }
 
 description = "utilitymodule"
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+repositories {
+    mavenCentral()
+}
+kotlin {
+    jvmToolchain(19)
+}
