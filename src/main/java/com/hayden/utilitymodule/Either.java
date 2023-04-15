c600ad4aa2ea39e6d729e50183201ff6946395fe
Hayden Rear <hayden.rear@gmail.com>
2023-04-15 19:04:32 -0400

    service loader for translators.

---
+++ src/main/java/com/hayden/utilitymodule/Either.java
@@ -0,0 +1,21 @@
+package com.hayden.utilitymodule;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Either<T,V> {
+
+    @Nullable
+    T left;
+    @Nullable
+    V right;
+
+    public static <T,V> Either<T,V> from(@Nullable T first, @Nullable V second) {
+        return new Either<>(first, second);
+    }
+}
