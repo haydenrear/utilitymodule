d0fa9b595582494f7626082252de273874d03d1c
hayden.rear <hayden.rear@gmail.com>
2023-12-02 11:29:47 -0500

    ByteDataCrawler.java updated streaming

--- src/main/java/com/hayden/utilitymodule/security/SignatureUtil.java
+++ src/main/java/com/hayden/utilitymodule/security/SignatureUtil.java
@@ -1,11 +1,19 @@
 package com.hayden.utilitymodule.security;
 
+import com.hayden.utilitymodule.io.FileUtils;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.ByteBuf;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
@@ -22,14 +30,27 @@ public class SignatureUtil {
         return TakeMessageDigest(input, null);
     }
 
+    public static Optional<byte[]> TakeMessageDigest(Path input, @Nullable String algorithm) {
+        return retrieveDigestAlgorithm(algorithm).flatMap(s -> {
+            try {
+                return Optional.of(MessageDigest.getInstance(s));
+            } catch (NoSuchAlgorithmException e) {
+                log.error("Could not retrieve message digest instance: {}", e.getMessage());
+            }
+            return Optional.empty();
+        }).flatMap(md -> {
+            try(InputStream is = Files.newInputStream(input); DigestInputStream ds = new DigestInputStream(is, md)) {
+                return Optional.of(ds.getMessageDigest().digest());
+            } catch (IOException e) {
+                log.error("Could not create digest input stream: {}.", e.getMessage());
+            }
+            return Optional.empty();
+        });
+    }
+
     public static Optional<byte[]> TakeMessageDigest(byte[] input, @Nullable String algorithm) {
-        Set<String> messageDigestAlgorithms = Security.getAlgorithms(MessageDigest.class.getSimpleName());
-        return messageDigestAlgorithms
-                .stream().filter(a -> a.equals(algorithm))
-                .findAny()
-                .or(() -> messageDigestAlgorithms.stream().filter(s -> s.equals("MD5"))
-                        .findAny()
-                )
+        Optional<String> retrieveDigestAlgorithm = retrieveDigestAlgorithm(algorithm);
+        return retrieveDigestAlgorithm
                 .flatMap(md5Algorithm -> {
                     try {
                         byte[] digest = MessageDigest.getInstance(md5Algorithm).digest(input);
@@ -39,6 +60,18 @@ public class SignatureUtil {
                     }
                     return Optional.empty();
                 });
+    }
+
+    @NotNull
+    private static Optional<String> retrieveDigestAlgorithm(@Nullable String algorithm) {
+        Set<String> messageDigestAlgorithms = Security.getAlgorithms(MessageDigest.class.getSimpleName());
+        Optional<String> retrieveDigestAlgorithm = messageDigestAlgorithms
+                .stream().filter(a -> a.equals(algorithm))
+                .findAny()
+                .or(() -> messageDigestAlgorithms.stream().filter(s -> s.equals("MD5"))
+                        .findAny()
+                );
+        return retrieveDigestAlgorithm;
     }
 
 }
