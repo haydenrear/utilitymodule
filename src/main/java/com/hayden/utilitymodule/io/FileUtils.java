817ee7a76b11d3250f90a86895cdc9f64c56080d
hayden.rear <hayden.rear@gmail.com>
2024-04-24 19:19:40 -0400

    changed ordering of pointwise

--- src/main/java/com/hayden/utilitymodule/io/FileUtils.java
+++ src/main/java/com/hayden/utilitymodule/io/FileUtils.java
@@ -1,10 +1,19 @@
 package com.hayden.utilitymodule.io;
 
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.Assert;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -46,34 +55,65 @@ public class FileUtils {
         }
     }
 
-    public static Stream<Path> GetFilesRecursive(Path path) {
-        try  {
-            var files = Files.list(path);
-            return files.flatMap(p -> {
-                if (p.toFile().isFile()) {
-                    return Stream.of(p);
-                } else if (p.toFile().isDirectory()) {
-                    return GetFilesRecursive(p);
-                }
+    public static boolean doOnFilesRecursive(Path path, Function<Path, Boolean> toDo) {
+        var did = Optional.ofNullable(path.toFile().listFiles())
+                .stream()
+                .flatMap(Arrays::stream)
+                .map(p -> {
+                    if (p.isFile()) {
+                        return toDo.apply(p.toPath());
+                    } else if (p.isDirectory()) {
+                        boolean didDelete = doOnFilesRecursive(p.toPath(), toDo);
+                        boolean deleteDir = toDo.apply(p.toPath());
+                        return didDelete && deleteDir;
+                    }
+                    return false;
+                })
+                .collect(Collectors.toSet());
+        Boolean didDeletePath = toDo.apply(path);
+        boolean didDeleteAll = did.stream().allMatch(Boolean::booleanValue);
+        return didDeletePath && didDeleteAll;
+    }
 
-                return Stream.empty();
-            });
-        } catch (IOException ignored) {
-            // ignore
-        }
-        return Stream.empty();
+    public static Stream<Path> GetFilesRecursive(Path path) {
+        Stream.Builder<Path> builder = Stream.builder();
+        doOnFilesRecursive(path, (p) -> {
+            builder.add(p);
+            return true;
+        });
+        return builder.build();
+    }
+
+    public static Stream<File> getFileStream(Path path) {
+        return Optional.ofNullable(path.toFile().listFiles())
+                .stream()
+                .flatMap(Arrays::stream);
+    }
+
+    public static boolean isEmpty(Path path) {
+        return getFileStream(path).findAny().isEmpty();
     }
 
     public static boolean deleteFilesRecursive(Path path) {
-        return GetFilesRecursive(path).allMatch(p -> {
-            try {
-                Files.delete(p);
+        return doOnFilesRecursive(path, p -> {
+            if (p.toFile().isDirectory()) {
+                Assert.assertTrue(isEmpty(p));
+                if (!p.toFile().delete()) {
+                    logNotDeleted(p);
+                    return false;
+                }
                 return true;
-            } catch (IOException e) {
-                log.error("Could not delete file {} with error {}.", p.toFile().getAbsoluteFile(), e.getMessage());
+            } else if (p.toFile().isFile() && !p.toFile().delete()) {
+                logNotDeleted(p);
+            } else {
                 return false;
             }
+            return false;
         });
+    }
+
+    private static void logNotDeleted(Path p) {
+        log.error("Could not delete {}.", p);
     }
 
 }
