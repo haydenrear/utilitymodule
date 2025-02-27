77922ffb4abe484b44ce2aa5a61e63399177d00f
hayden.rear <hayden.rear@gmail.com>
2025-02-27 17:00:28 -0500

    add ing commit diff to blame tree

---
+++ src/main/java/com/hayden/utilitymodule/config/EnvConfigProps.java
@@ -0,0 +1,18 @@
+package com.hayden.utilitymodule.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Path;
+
+@ConfigurationProperties(prefix = "env-props")
+@Component
+@Data
+public class EnvConfigProps {
+
+    Path homeDir;
+
+    Path projectDir;
+
+}
