2c23258988aee619801d734e24f27808a14aa2fd
hayden.rear <hayden.rear@gmail.com>
2025-03-22 19:55:01 -0400

    Lazy delegating iterator

---
+++ src/test/java/com/hayden/utilitymodule/iter/LazyDelegatingIteratorTest.java
@@ -0,0 +1,41 @@
+package com.hayden.utilitymodule.iter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
+class LazyDelegatingIteratorTest {
+
+    @Test
+    public void testLazyDelegatingIterator() {
+        LazyDelegatingIterator<Integer> iterator = new LazyDelegatingIterator<>(
+                IntStream.range(0, 10).boxed().map(i -> IntStream.range(0, i).boxed().toList().iterator())
+                        .toList(),
+                IntStream.range(0, 10).boxed().toList().iterator());
+
+        int count = 0;
+        while(iterator.hasNext()) {
+            var next = Assertions.assertDoesNotThrow(iterator::next);
+            log.info("{}", next);
+            count += 1;
+        }
+
+        Assertions.assertEquals(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10, count);
+
+        var nIterator = new LazyDelegatingIterator<Integer>(
+                new ArrayList<>(),
+                List.<Integer>of().iterator());
+        count = 0;
+        while (nIterator.hasNext()) {
+            count += 1;
+        }
+
+        Assertions.assertEquals(0, count);
+    }
+
+}
