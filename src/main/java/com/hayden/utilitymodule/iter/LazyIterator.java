0d297d21163219574e9395310d3b9e2bd2b6ce2e
hayden.rear <hayden.rear@gmail.com>
2025-03-22 20:00:18 -0400

    Lazy delegating iterator

---
+++ src/main/java/com/hayden/utilitymodule/iter/LazyIterator.java
@@ -0,0 +1,61 @@
+package com.hayden.utilitymodule.iter;
+
+import com.google.common.collect.Lists;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class LazyIterator<T extends LazyIterator.HasChildren<T>> implements Iterator<T> {
+
+    public interface HasChildren<T> {
+        Iterator<T> childrenIter();
+    }
+
+    private final List<T> receivedChildren;
+    private final Deque<T> futureNodes;
+    private int currentIndex;
+
+    public LazyIterator(List<T> receivedChildren, int recP) {
+        this.receivedChildren = receivedChildren;
+        this.futureNodes = new ArrayDeque<>();
+        this.currentIndex = recP + 1;
+        this.addNextFutureNode();
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return !futureNodes.isEmpty() || currentIndex < receivedChildren.size();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException("No more elements to iterate.");
+        }
+
+
+        if (!futureNodes.isEmpty()) {
+            return futureNodes.pop();
+        }
+
+        addNextFutureNode();
+
+        return this.futureNodes.pop();
+    }
+
+    private @NotNull void addNextFutureNode() {
+        T nextNode = receivedChildren.get(currentIndex);
+        futureNodes.add(nextNode);
+        Iterator<T> fileNodeIterator = nextNode.childrenIter();
+        if (fileNodeIterator.hasNext())
+            futureNodes.addAll(Lists.newArrayList(fileNodeIterator));
+        currentIndex += 1;
+    }
+
+    public void reset() {
+        currentIndex = 0;
+        futureNodes.clear();
+    }
+
+}
