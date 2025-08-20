e1a9f61e1a9a81173a8335a46aa439f75a595ed2
hayden.rear <hayden.rear@gmail.com>
2025-08-20 11:25:50 -0400

    test mcp server

--- src/main/java/com/hayden/utilitymodule/proxies/ProxyUtil.java
+++ src/main/java/com/hayden/utilitymodule/proxies/ProxyUtil.java
@@ -1,13 +1,31 @@
 package com.hayden.utilitymodule.proxies;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.AopProxy;
+import org.springframework.aop.framework.AopProxyUtils;
+import org.springframework.cglib.proxy.Proxy;
 
+import java.util.Objects;
+
+@Slf4j
 @UtilityClass
 public class ProxyUtil {
 
     public boolean isProxy(Object obj) {
         if (obj instanceof Class c)
             throw new RuntimeException("Attempted to check if class class was proxy class!");
+
+        var aopProxy = AopProxyUtils.ultimateTargetClass(obj);
+        if (!Objects.equals(obj.getClass(), aopProxy)) {
+            return true;
+        }
+        var i = Proxy.isProxyClass(obj.getClass());
+
+        if (i)
+            return true;
+
+        log.error("Didn't check JDK proxy!");
         return false;
     }
 
