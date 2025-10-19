3ceac16937b9c982c5e6fabd94a6f586482ea595
hayden.rear <hayden.rear@gmail.com>
2025-10-19 13:11:16 -0400

    updated disable otel

---
+++ src/main/java/com/hayden/utilitymodule/otel/DisableOtelConfiguration.java
@@ -0,0 +1,34 @@
+package com.hayden.utilitymodule.otel;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+@ConditionalOnProperty(value = "management.tracing.enabled", havingValue = "false")
+public class DisableOtelConfiguration implements EnvironmentPostProcessor {
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment env, SpringApplication application) {
+        if(env.matchesProfiles("telemetry-logging")) {
+            log.info("OTEL SDK is not being disabled.");
+            return;
+        }
+
+        boolean tracingEnabled = env.getProperty("management.tracing.enabled", Boolean.class, false);
+
+        if (!tracingEnabled) {
+            log.info("OTEL SDK is being disabled.");
+            env.getPropertySources().addFirst(
+                    new MapPropertySource("otel-disable",
+                            Map.of("otel.sdk.disabled", "true"))
+            );
+        }
+    }
+}
