1e5a8e90e2fd029aefed80469298153292d3cc2d
hayden.rear <hayden.rear@gmail.com>
2025-10-31 15:37:30 -0400

    updating to fix issues with concurrency and spoon launcher when passing in src/main/java

--- src/main/java/com/hayden/utilitymodule/reflection/ParameterAnnotationUtils.java
+++ src/main/java/com/hayden/utilitymodule/reflection/ParameterAnnotationUtils.java
@@ -8,9 +8,25 @@ import org.springframework.core.annotation.AnnotatedElementUtils;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public interface ParameterAnnotationUtils {
+
+    public record AnnotatedArg<T extends Annotation>(Integer idx, T t) {}
+
+    static <T extends Annotation> List<AnnotatedArg<T>> retrieveArgsIndex(ProceedingJoinPoint pjp, Class<T> annotationClazz) {
+        MethodSignature sig = (MethodSignature) pjp.getSignature();
+        Method method = sig.getMethod();
+        Class<?> targetClass = pjp.getTarget() != null ? pjp.getTarget().getClass() : sig.getDeclaringType();
+        Method specific = AopUtils.getMostSpecificMethod(method, targetClass);
+
+        // 2) Locate any parameter annotated with @SessionVariable
+        return findAllAnnotatedParameterIndex(specific, annotationClazz);
+    }
 
     static Optional<Integer> retrieveArgIndex(ProceedingJoinPoint pjp, Class<? extends Annotation> annotationClazz) {
         // 1) Resolve the concrete Method (handle proxies)
@@ -21,6 +37,22 @@ public interface ParameterAnnotationUtils {
 
         // 2) Locate any parameter annotated with @SessionVariable
         return findFirstAnnotatedParameterIndex(specific, annotationClazz);
+    }
+
+    private static <T extends Annotation> List<AnnotatedArg<T>> findAllAnnotatedParameterIndex(Method m, Class<T> annoType) {
+        Parameter[] params = m.getParameters();
+
+        return IntStream.range(0, params.length)
+                .boxed()
+                .flatMap(i -> {
+                    T mergedAnnotation = AnnotatedElementUtils.findMergedAnnotation(params[i], annoType);
+                    if (mergedAnnotation != null) {
+                        return Stream.of(new AnnotatedArg<T>(i, mergedAnnotation));
+                    }
+
+                    return Stream.empty();
+                })
+                .toList();
     }
 
     /** Returns the first parameter index annotated with the given annotation type. */
